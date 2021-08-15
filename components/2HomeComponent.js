5acae1d9db644cc7195a011c45b4e0e6e95133e6
jackie-shawley <jackiekopco@aol.com>
2021-08-15 17:36:31 -0400

    Home

--- components/2HomeComponent.js
+++ components/2HomeComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
-import { View, StyleSheet} from 'react-native';
+import { View, StyleSheet, FlatList, TouchableOpacity, Text, ScrollView } from 'react-native';
 import { HomePhotos } from '../shared/HomePhotosData';
 import { Card, Title, Paragraph } from 'react-native-paper';
-import { MENU } from '../shared/MenuData';
+import { Image, Tile } from 'react-native-elements';
+import IngredientsDirectory from './IngredientsDirectory';
 
 
 
@@ -18,32 +18,44 @@ class Home extends Component {
 
     render() {
         return(
-            <View style={styles.container}>
-                <View>
-                    <View>
-                        <FlatList
-                            data={this.state.photos}
-                            renderItem={({item}) => 
-                                <View style={{flex: 1, padding: 10}}>
-                                    <Card>
-                                        <Card.Cover source={item.image} />
-                                        <Card.Content>
-                                            <Title style={styles.cardTitle}>{item.name}</Title>
-                                            <Paragraph style={styles.text}>{item.description}</Paragraph>
-                                        </Card.Content>
-                                    </Card>
-                                </View>
-                            }
-
-                            keyExtractor={item => item.id.toString()}
-                        />
+            <ScrollView style={styles.container}>
+                <View >
+                    <View style={styles.cardContainer}>
+                        <Card elevation={3}>
+                            <Card.Cover source={ require('../assets/images/cakes/tiered-blue-cake.jpeg') } />
+                            <Card.Content>
+                                <Title style={styles.cardTitle}>Custom Cakes For Any Occasion</Title>
+                                <Paragraph style={styles.text}>No more missing out on a delicious cake for your birthday! We're happy to design a sheet cake or a layer cake for any occasion.
+                                </Paragraph>
+                            </Card.Content>
+                        </Card>
+                    </View>
+                    <View style={styles.cardContainer}>
+                        <Card elevation={3}>
+                            <Card.Cover source={ require('../assets/images/cookies/silly-candy-corns-for-web.jpeg') } />
+                            <Card.Content>
+                                <Title style={styles.cardTitle}>Cookies Made Just For YOU</Title>
+                                <Paragraph style={styles.text}>Silly...pretty...funny...we'll customize your cookies any way you like! Order two weeks ahead to ensure availability, or check our case if you want them today!
+                                </Paragraph>
+                            </Card.Content>
+                        </Card>
+                    </View>
+                    <View style={styles.cardContainer}>
+                        <Card elevation={3}>
+                            <Card.Cover source={ require('../assets/images/cafe/circle-of-drinks-cropped.png') } />
+                            <Card.Content>
+                                <Title style={styles.cardTitle}>Organic Fair Trade Coffees and Teas</Title>
+                                <Paragraph style={styles.text}>Come in to our cafe and have a choose from our carefully curated selection of organic loose-leaf teas, or try a cup of our shade-grown, fairtrade, locally-roasted coffee. Stay and relax, or take one to go.</Paragraph>
+                            </Card.Content>
+                        </Card>
                     </View>
                 </View>
-            </View>
+            </ScrollView>
         );
     }
 }
 
+ 
 
 const styles = StyleSheet.create({
     container: {
@@ -58,12 +70,15 @@ const styles = StyleSheet.create({
     },
     cardTitle:{
         fontFamily: 'Rochester',
-        fontSize: 22,
-        fontWeight: 'bold',
+        fontSize: 28,
         flex: 1,
-        textAlign: 'center'
-        
+        textAlign: 'center',
+        marginTop: 10
     },
+    cardContainer: {
+        margin: 20,
+
+    }
 });
 
 export default Home;
--- shared/HomePhotosData.js
+++ shared/HomePhotosData.js
@@ -1,8 +1,9 @@
+
 export const HomePhotos = [
     {
         id: 0,
         name: 'Custom Cakes for Any Occasion',
-        image: "../assets/images/cakes/tiered-blue-cake.jpeg",
+        image: "/Users/jackiekopco/Desktop/NucampFolder/React Native BHGF.nosync/bhgf-react-native/assets/images/cakes/tiered-blue-cake.jpeg",
         description: 'No more missing out on a delicious cake for your birthday! We\'re happy to design a sheet cake or a layer cake for any occasion.' 
     },
     {
@@ -14,7 +15,7 @@ export const HomePhotos = [
     {
         id: 2,
         name: 'Organic Fair Trade Coffees and Teas',
-        image: "/assets/images/cafe/circle-of-drinks-cropped.png",
+        image: '/assets/images/cafe/circle-of-drinks-cropped.png',
         description: 'If you\'re looking for something rich, decadent and so, so satisfying, this is the cake for you. Made with espresso in the cake and the icing, as well as two kinds of real cocoa, chocolate chips and chocolate chunks, this cake is a chocolate lover\'s dream come true.' 
     },
 ]
